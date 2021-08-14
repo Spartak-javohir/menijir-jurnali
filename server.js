e1bba4426db09b7ccbc8ad6bbc9be84a585a1cb7
Spartak-javohir <dellspartak00@gmail.com>
2021-08-14 12:09:08 +0500

    add express and change server.js and change index.html

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const fs = require('fs').promises
+
+
+ const app = express();
+ app.listen(3030, ()=>{
+     console.log('app running at 3030 port');
+ })
+
+ app.use(express.static(__dirname+'/public'))
+ app.get('/', async (req, res)=>{
+    let reed = await fs.readFile(__dirname+'/views/index.html', 'utf-8')
+     res.send(reed)
+ })
